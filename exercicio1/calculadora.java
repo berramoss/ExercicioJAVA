e32444c278f70d136ccf63ef3328389e1567b9f1
Bernardo Ramos <168155547+berramoss@users.noreply.github.com>
2025-04-09 13:53:04 -0300

    Create calculadora.java

---
+++ exercicio1/calculadora.java
@@ -0,0 +1,21 @@
+public class calculadora {
+
+    public static double soma(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtracao(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplicacao(double a, double b) {
+        return a * b;
+    }
+
+    public static double divisao(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Erro: divisão por zero.");
+        }
+        return a / b;
+    }
+}
